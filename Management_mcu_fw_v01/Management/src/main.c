3c214dcc6b3cde2b83dbb5c2f55c1b536be36f58
Luca Schettini <luca.schettini@sanitaseg.it>
2018-03-20 16:20:01 +0100

    ADC voltages read OK!

--- Management_mcu_fw_v01/Management/src/main.c
+++ Management_mcu_fw_v01/Management/src/main.c
@@ -3,20 +3,26 @@
  */
 #include <asf.h>
 
+float voltages[12] = { 0 };
+	
+const float voltagesMot[] = { 0.4395, 0.4395, 0.4395, 0.7396, 0.4395,  0.4395, 0.9763, 0.4395, 0.9763, 1.4793, 0.8766, 0.8284 };
+
 static void configure_adc(void)
 {
 	/* List ADC Channel*/
-	enum adc_channel_num_t ch_list[10] = {
+	enum adc_channel_num_t ch_list[12] = {
 		ADC_SOC,
 		ADC_ARM,
 		ADC_DDR,
 		ADC_2V5,
 		ADC_1V0,
 		ADC_1V1,
-		ADC_1V2, 
+		ADC_VCORE, 
 		ADC_1V5, 
 		ADC_3V3, 
-		ADC_5V	
+		ADC_5V,
+		ADC_3V,
+		ADC_2V8
 		};
 	/* Configure ADC pin for light sensor. */
 	gpio_configure_pin(PIO_PB0_IDX, PIO_INPUT);
@@ -27,7 +33,7 @@ static void configure_adc(void)
 	/* Configure ADC. */
 	adc_init(ADC, sysclk_get_cpu_hz(), 1000000, ADC_MR_STARTUP_SUT0);
 	//adc_enable_channel(ADC, ADC_CHANNEL_0);
-	for (int i = 0; i < 10; i++) {
+	for (int i = 0; i < 12; i++) {
 		adc_enable_channel(ADC, (enum adc_channel_num_t)i);
 	}
 	adc_configure_trigger(ADC, ADC_TRIG_SW, 1);
@@ -35,17 +41,24 @@ static void configure_adc(void)
 
 void readADC(void)
 {
-	uint16_t adc_value;
-	uint16_t normalized_adc_value;
+	uint32_t adc_value;
+	uint32_t normalized_adc_value;
 	// Test ADC
 	//adc_val = adc_get_channel_value(ADC, ADC_CHANNEL_0);
-			
-	uint16_t adc_value = 0;
-	for(int i=0; i<10; i++)
-	adc_value += adc_get_channel_value(ADC, ADC_CHANNEL_0);
-	adc_value /= 10;
-	//normalize
-	normalized_adc_value = (adc_value * 1000) / 4096;
+	
+	
+	for(int y=0; y < 12; y++)
+	{	
+		adc_value = 0;
+		for(int i=0; i < 10; i++)
+		adc_value += adc_get_channel_value(ADC,  (enum adc_channel_num_t)y);
+		adc_value /= 10;
+		//normalize
+		//normalized_adc_value = (adc_value * 1000) / 4096; // Da sistemare in base al partitore
+		voltages[y] = (adc_value * voltagesMot[y]);
+	}
+	int b = 0;
+	return;
 }
 
 int main (void)
